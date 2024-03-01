51810854991cd7a8c4602043fe75a0fab2294341
ARadRareness <38016746+ARadRareness@users.noreply.github.com>
2024-03-01 22:20:08 +0100

    Add system message support to gradio client

--- client/grad_client.py
+++ client/grad_client.py
@@ -7,19 +7,23 @@ conversation_id = client_api.start_conversation()
 
 with gr.Blocks() as demo:
     chatbot = gr.Chatbot()
-    msg = gr.Textbox(lines=5)
+    system_message_box = gr.Textbox(lines=2, label="System Message")
+    message_box = gr.Textbox(lines=5, label="User Message")
 
-    def respond(message: str, chat_history: List[tuple[str, str]]):
+    def respond(message: str, chat_history: List[tuple[str, str]], system_message: str):
+        if system_message:
+            client_api.add_system_message(conversation_id, system_message)
+
         response = client_api.generate_response(conversation_id, message)
 
         chat_history.append((message, response))
         return "", chat_history
 
     with gr.Row():
-        clear = gr.ClearButton([msg, chatbot])
+        clear = gr.ClearButton([message_box, system_message_box, chatbot])
         submit = gr.Button("Submit")
-        submit.click(respond, [msg, chatbot], [msg, chatbot])  # type: ignore
+        submit.click(fn=respond, inputs=[message_box, chatbot, system_message_box], outputs=[message_box, chatbot])  # type: ignore
 
-    msg.submit(respond, [msg, chatbot], [msg, chatbot])  # type: ignore
+    message_box.submit(fn=respond, inputs=[message_box, chatbot, system_message_box], outputs=[message_box, chatbot])  # type: ignore
 
     demo.launch()  # type: ignore
